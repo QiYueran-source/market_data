9b88f5e8edc5d49a2fa592e7cbdf288fc482b0a1
frank <1206585163@qq.com>
2026-04-05 09:50:05 +0800

    交易日历异常优化

--- exceptions/api_error/stock_api_error.py
+++ exceptions/api_error/stock_api_error.py
@@ -1,20 +1,23 @@
 from exceptions.api_error.base_error import ApiError
 
-# -------------------- stock_api --------------------
 class StockApiError(ApiError):
     '''stock_api错误基类'''
 
-class StockApiQuotaExhaustedError(StockApiError):
+# -------------------- 交易日历 --------------------
+class TradeCalendarError(StockApiError):
+    '''交易日历错误基类'''
+
+class StockApiQuotaExhaustedError(TradeCalendarError):
     '''请求次数超过限额'''
 
-class UnexpectedApiCodeError(StockApiError):
+class UnexpectedApiCodeError(TradeCalendarError):
     '''返回了意外的API码'''
 
-class DataEmptyError(StockApiError):
+class DataEmptyError(TradeCalendarError):
     '''数据为空'''
 
-class WrongDataError(StockApiError):
+class WrongDataError(TradeCalendarError):
     '''数据格式错误'''
 
-class WrongIsOpenRangeError(StockApiError):
+class WrongIsOpenRangeError(TradeCalendarError):
     '''is_open范围错误'''
