fd206153bd71d27105f050852737e7fecc33e959
frank <1206585163@qq.com>
2026-04-09 14:28:45 +0800

    移动schema_map,避免远程使用api时报错

--- build_database.py
+++ build_database.py
@@ -24,7 +24,16 @@ logger = get_logger(name='build_database')
 
 # 常量
 from db import DB_DIR
-from db import DB_SCHEMAS
+from schema.trade_calendar import TRADE_CALENDAR_SCHEMAS_LIST
+from schema.security_info import SECURITY_INFO_SCHEMAS_LIST
+from schema.minutely_trade_data import MINUTELY_TRADE_DATA_SCHEMAS_LIST
+from schema.daily_trade_data import DAILY_TRADE_DATA_SCHEMAS_LIST
+DB_SCHEMAS = {
+    'trade_calendar.db': TRADE_CALENDAR_SCHEMAS_LIST,
+    'security_info.db': SECURITY_INFO_SCHEMAS_LIST,
+    'minutely_trade_data.db': MINUTELY_TRADE_DATA_SCHEMAS_LIST,
+    'daily_trade_data.db': DAILY_TRADE_DATA_SCHEMAS_LIST
+} # 表结构列表
 
 
 def _sqlite_supports_drop_column() -> bool:
